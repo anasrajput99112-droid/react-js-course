3986f157406e012a1bed185f5172bdb6a4f7e2a2
Anas <anasrajput99112@gmail.com>
2026-04-14 19:45:19 +0500

    learing reactjs

---
+++ reactjs/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+const App = () => {
+  return (
+    // React Fragment
+    <> 
+      <header className="flex items-center justify-between p-4 bg-gray-800 text-white">
+        <div>
+          <h1>logo</h1>
+        </div>
+        <nav>
+          <a href="" className="hover:underline">Home</a>
+          <a href="" className="hover:underline">About</a>
+          <a href="" className="hover:underline">Services</a>
+          <a href="" className="hover:underline">Contact</a>
+        </nav>
+      </header>
+      
+    </>
+  )
+}
+
+
+
+
+export default App
