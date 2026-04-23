9fc078cad718bf23ca3e50f45c0c1539324c3286
Anas <anasrajput99112@gmail.com>
2026-04-23 17:46:07 +0500

    working with function

--- 08-function/src/App.jsx
+++ 08-function/src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 
 const App = () => {
-
-  function onScroll(){
-    console.log('scroll');
-    
-  }
-
   return (
-    <div onWheel={onScroll}>
-     <div className="page1"></div>
-     <div className="page2"></div>
-     <div className="page3"></div>
+    <div>
+      <button>Click</button>
     </div>
   )
 }
