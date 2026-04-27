8389e6adb77710bca8288f960d86e7c57e7d09d3
Anas <anasrajput99112@gmail.com>
2026-04-27 15:19:01 +0500

    local storage medthod

---
+++ 14-localstorage/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  // localStorage.setItem("user", "anas");
+  // localStorage.setItem("age", "19");
+
+  // const age = localStorage.getItem('age')
+  //   console.log(age);
+
+  // const user = {
+  //   username:'Anas',
+  //   age:18,
+  //   city:'karachi'
+  // }
+    
+  // localStorage.setItem('user',JSON.stringify(user))
+  
+
+  return <div></div>;
+};
+
+export default App;
